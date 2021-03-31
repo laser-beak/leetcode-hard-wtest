866638e9f1d177d8b5c7c348cff715c59e763d11
Anvarov Almaz <almaz.anvarov@aplana.com>
2021-03-31 10:33:05 +0300

    leetcode-hard-wtests 2 no test found fixed

--- src/test/java/ru/aincorp/leetcode/problems/hard/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ru/aincorp/leetcode/problems/hard/MedianOfTwoSortedArraysTest.java
@@ -7,12 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MedianOfTwoSortedArraysTest {
 
     @Test
-    private void result_zero_when_array_nums_of_zero() {
+    void findMedianSortedArrays() {
+
+    }
+
+    @Test
+    void find_Median_Sorted_Arrays_When_Zero_Arrays() {
         MedianOfTwoSortedArrays medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
         int[] num1 = {0, 0};
         int[] num2 = {0, 0};
 
         assertEquals(0.00000, medianOfTwoSortedArrays.findMedianSortedArrays(num1, num2));
     }
-
 }
